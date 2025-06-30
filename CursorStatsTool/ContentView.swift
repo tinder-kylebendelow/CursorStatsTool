fed0c9e4ee2507c7e6a6cebeae971c7049f61265
Kyle Bendelow <kyle.bendelow@gotinder.com>
2025-06-30 13:19:17 -0500

    Initial Commit

---
+++ CursorStatsTool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CursorStatsTool
+//
+//  Created by Kyle Bendelow on 6/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CursorStatsTool/CursorStatsToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorStatsToolApp.swift
+//  CursorStatsTool
+//
+//  Created by Kyle Bendelow on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct CursorStatsToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
